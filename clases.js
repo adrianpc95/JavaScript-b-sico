7ef21074a7eca7c21bda1b997a65544e3438ad20
Adrianpc95 <adrianpinol95ns@gmail.com>
2024-05-07 18:55:16 +0200

    Herencias

--- clases.js
+++ clases.js
@@ -1,8 +1,8 @@
 class Coche {
-    constructor(){
-        this.modelo = '';
-        this.velocidad = '';
-        this.antiguedad = '';
+    constructor(modelo, velocidad, antiguedad){
+        this.modelo = modelo;
+        this.velocidad = velocidad;
+        this.antiguedad = antiguedad;
     }
 
     aumentarVelocidad(){
@@ -13,6 +13,21 @@ class Coche {
         this.velocidad -= 1;
     }
 }
+
+class Autobus extends Coche{
+    constructor(modelo, velocidad, antiguedad){
+        super(modelo, velocidad, antiguedad);
+        this.altura = 5;
+    }
+
+    mostrarAltura(){
+        return "La altura del bus es: "+this.altura;
+    }
+}
+
+var autobus1 = new Autobus('Pegasus', 200, 2017);
+console.log(autobus1.mostrarAltura());
+
 
 var coche1 = new Coche('BMW', 200, 2017);
 var coche2 = new Coche('Audi', 100, 2018);
